b6c8d1adc97259a8b9a51840ba4ca1c6ebacc28a
DIOGO SILVEIRA MENDONCA <diogosmendonca@gmail.com>
2020-12-17 20:20:27 -0300

    user, login e sessão

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var FileStore = require('session-file-store')(session);
 var indexRouter = require('./routes/index');
 var projetosRouter = require('./routes/projetos');
 var atividadesRouter = require('./routes/atividades');
+var usersRouter = require('./routes/users');
 
 const mongoose = require('mongoose');
 
@@ -34,42 +35,26 @@ app.use(session({
 }));
 
 
+//app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth (req, res, next) {
   console.log(req.session);
 
-  if (!req.session.user) {
-    var authHeader = req.headers.authorization;
-    if (!authHeader) {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');
-        err.status = 401;
-        next(err);
-        return;
-    }
-
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-    var user = auth[0];
-    var pass = auth[1];
-    if (user == 'admin' && pass == 'password') {
-        req.session.user = 'admin';
-        next(); // authorized
-    } else {
-        var err = new Error('You are not authenticated!');
-        res.setHeader('WWW-Authenticate', 'Basic');      
-        err.status = 401;
-        next(err);
-    }
-  }else{
-    if (req.session.user === 'admin') {
-      console.log('req.session: ',req.session);
+  if(!req.session.user) {
+    var err = new Error('You are not authenticated!');
+    err.status = 403;
+    return next(err);
+  } else {
+    if (req.session.user === 'authenticated') {
       next();
-    }
-    else {
-        var err = new Error('You are not authenticated!');
-        err.status = 401;
-        next(err);
+    } else {
+      var err = new Error('You are not authenticated!');
+      err.status = 403;
+      return next(err);
     }
   }
+  
 }
 
 app.use(auth);
@@ -77,7 +62,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/projetos', projetosRouter);
 app.use('/atividades', atividadesRouter);
 
